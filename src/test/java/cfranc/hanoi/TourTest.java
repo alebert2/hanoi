c6d147b8d98ce4604e82496bfa4cd99552932abe
alebert2 <alebert2.beaumont@laposte.net>
2018-01-29 16:30:35 +0100

    Add files via upload

--- src/test/java/cfranc/hanoi/TourTest.java
+++ src/test/java/cfranc/hanoi/TourTest.java
@@ -8,7 +8,21 @@ public class TourTest {
 
 	@Test
 	public void test() {
-
+		
+	}
+	
+	public void testEmpiler() {
+		// But : Test la fonction empiler de Tour.java
+		// Entrée : néant
+		// Sortie : néant
+		
+		// Variables
+		int disquePetit = 1;
+		int disqueGrand = 2;
+		boolean disquesEmpty = true;
+		boolean disquesNotEmpty = false;
+		boolean tailleMin = true;
+		boolean tailleMaxSuperieure = false;
 	}
 
 }
